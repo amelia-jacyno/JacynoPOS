855cb33cffcb28be59b333172b7c4a39e6be9216
Adam Jacyno <jacynoadam@gmail.com>
2019-12-21 11:33:24 +0100

    Refactor: Change <br> to margins in deletion popups

--- application/views/ajax/popup/confirm_delete_order_item.php
+++ application/views/ajax/popup/confirm_delete_order_item.php
@@ -1,8 +1,7 @@
 <div class="popup-bg center-content">
 	<div class="popup p-4">
 		<div>Czy na pewno chcesz usunąć ten przedmiot?</div>
-		<br>
-		<div class="text-center text-light">
+		<div class="text-center text-light mt-3">
 			<a onclick="delete_order_item(<?= $order_id . ', ' . $order_item_id ?>); close_popup()" class="btn btn-primary mr-2">Tak</a>
 			<a onclick="close_popup()" class="btn btn-danger">Nie</a>
 		</div>
