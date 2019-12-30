67ddce71809c9b695d3f5ae84e10a50c9c400d2d
Adam Jacyno <jacynoadam@gmail.com>
2019-12-30 19:21:08 +0100

    refactor: make main_menu category list row a separate file

--- application/views/ajax/main_menu.php
+++ application/views/ajax/main_menu.php
@@ -25,43 +25,9 @@
         <hr class="m-0">
         <?php
         foreach ($orders as $order) {
-            $collapse_id = 'order-info-' . $order->order_id ?>
-            <div class="row text-center">
-                <div class="col" onclick="trigger_collapse('<?= $collapse_id ?>')"><?= $order->order_id ?></div>
-                <div class="col" onclick="trigger_collapse('<?= $collapse_id ?>')"><?= $order->order_table ?></div>
-                <div class="col" onclick="trigger_collapse('<?= $collapse_id ?>')"><?= $order->order_time ?></div>
-                <div class="col">
-                    <a onclick="load_order_menu(<?= $order->order_id ?>)" href="#" class="btn btn-primary w-100 h-100 rounded-0">
-                        <i class="far fa-sticky-note"></i>
-                    </a>
-                </div>
-                <div class="col-12 collapse text-left" id=<?= $collapse_id ?>>
-					<div class="m-2">
-						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad
-						squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt
-						sapiente ea proident.
-					</div>
-                    <div class="row no-gutters">
-                        <div class="col-4">
-                            <a href="#" class="btn btn-primary w-100 rounded-0">
-                                <i class="far fa-sticky-note text-light"></i>
-                            </a>
-                        </div>
-                        <div class="col-4">
-                            <a href="#" class="btn btn-danger w-100 rounded-0">
-                                <i class="far fa-sticky-note text-light"></i>
-                            </a>
-                        </div>
-                        <div class="col-4">
-                            <a href="#" class="btn btn-warning w-100 rounded-0">
-                                <i class="far fa-sticky-note text-light"></i>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <hr class="m-0">
-            <?php
+			$data['order'] = $order;
+            $data['collapse_id'] = 'order-info-' . $order->order_id;
+            $this->view('ajax/main_menu/order_list_row', $data);
         }
         ?>
     </div>
