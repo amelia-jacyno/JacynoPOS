2197bffe99503b56d0eb070faf086ecec3c33af7
Adam Jacyno <jacynoadam@gmail.com>
2019-12-30 19:49:22 +0100

    ref: add missing file from previous commits

---
+++ application/views/ajax/main_menu/order_list_row.php
@@ -0,0 +1,34 @@
+<div id="order-row-<?= $order->order_id ?>" class="row text-center">
+	<div class="col" onclick="trigger_collapse('<?= 'order-info-' . $order->order_id ?>')"><?= $order->order_id ?></div>
+	<div class="col" onclick="trigger_collapse('<?= 'order-info-' . $order->order_id ?>')"><?= $order->order_table ?></div>
+	<div class="col" onclick="trigger_collapse('<?= 'order-info-' . $order->order_id ?>')"><?= $order->order_time ?></div>
+	<div class="col">
+		<a onclick="load_order_menu(<?= $order->order_id ?>)" href="#" class="btn btn-primary w-100 h-100 rounded-0">
+			<i class="far fa-sticky-note"></i>
+		</a>
+	</div>
+	<div class="col-12 collapse text-left" id=<?= 'order-info-' . $order->order_id ?>>
+		<div class="m-2">
+			Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad
+			squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt
+			sapiente ea proident.
+		</div>
+		<div class="row no-gutters">
+			<div class="col-4">
+				<a href="#" class="btn btn-primary w-100 rounded-0">
+					<i class="far fa-sticky-note text-light"></i>
+				</a>
+			</div>
+			<div class="col-4">
+				<a href="#" class="btn btn-danger w-100 rounded-0">
+					<i class="far fa-sticky-note text-light"></i>
+				</a>
+			</div>
+			<div class="col-4">
+				<a href="#" class="btn btn-warning w-100 rounded-0">
+					<i class="far fa-sticky-note text-light"></i>
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
