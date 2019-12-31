3874dd734917e6285fbc843005b0ce7319693781
Adam Jacyno <jacynoadam@gmail.com>
2019-12-31 16:57:34 +0100

    ref: change edit_order_menu item display

--- application/views/ajax/edit_order_menu.php
+++ application/views/ajax/edit_order_menu.php
@@ -10,20 +10,20 @@
 <div class="col-12 h-100 overflow-scroll">
 	<?php foreach ($order_items as $item) { ?>
 		<div class="row no-gutters">
-			<div class="col-9 p-1"
+			<div class="col-10 p-1"
 				 onclick="load_edit_item_form(<?= $item->order_id . ", " . $item->item_id ?>)">
 				<div>
-					<div><b><?= $item->item_name ?></b></div>
+					<div><b><?= $item->item_count . " x " . $item->item_name ?></b></div>
 				</div>
 				<div>
-					<?= $item->item_count . " x " . $item->item_price ?>
-					<b class="float-right mr-2"><?= $item->price . " zł" ?></b>
+					Cena: <?= $item->item_price ?> zł
+					<b class="float-right mr-2"><?= $item->price?> zł</b>
 				</div>
 				<div>
 					<?= $item->comment ?>
 				</div>
 			</div>
-			<div class="col-3">
+			<div class="col-2">
 				<a onclick="confirm_delete_order_item(<?= $item->order_id . ", " . $item->item_id ?>)" href="#"
 				   class="btn btn-danger w-100 h-100 rounded-0 center-content">
 					<i class="far fa-trash-alt fa-sticky-note text-light"></i>
