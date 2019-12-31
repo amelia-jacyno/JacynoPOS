e4d61fe6b8a159077a36935919defe984d2fe758
Adam Jacyno <jacynoadam@gmail.com>
2019-12-31 22:02:09 +0100

    ref: set main_menu % sizes and improve main_menu UI

--- application/views/main_menu/bottom_menu.php
+++ application/views/main_menu/bottom_menu.php
@@ -1,10 +1,10 @@
-<div class="row h-auto fixed-bottom center-content">
-	<div class="col-8 col-sm-6">
-		<div class="input-group mb-3 px-2">
-			<input type="text" class="form-control" id="table-input" placeholder="Stolik">
-			<div class="input-group-append">
-				<button onclick="add_order()" type="button" class="btn btn-primary">Dodaj</button>
-			</div>
+<div class="row no-gutters fixed-bottom center-content h-10">
+	<div class="col-6 h-100 p-0">
+		<div class="input-group h-100">
+			<input type="text" class="form-control rounded-0 w-100" id="table-input" placeholder="Stolik">
 		</div>
+	</div>
+	<div class="col-6 h-100 p-0">
+		<a onclick="add_order()" href="#" class="btn btn-primary w-100 h-100 rounded-0 center-content">Dodaj</a>
 	</div>
 </div>
--- application/views/main_menu/top_menu.php
+++ application/views/main_menu/top_menu.php
@@ -1,3 +1,3 @@
-<div class="row center-content m-4">
+<div class="row center-content h-15">
 	<h1>Lista zamówień</h1>
 </div>
