7bc3b17d5e90d683446db44598db16e7bb80fcbd
Adam Jacyno <jacynoadam@gmail.com>
2020-07-04 17:45:57 +0200

    fix: fix button texts not wrapping

--- application/views/waiter/item_menu/item_list.php
+++ application/views/waiter/item_menu/item_list.php
@@ -11,14 +11,14 @@
 		<div class="row no-gutters">
 			<?php foreach ($categories as $category) { ?>
 				<div class="col-4 col-sm-3 col-md-2 p-1">
-					<a href=#" onclick="load_item_list(<?= $category->category_id ?>)"
+					<a onclick="load_item_list(<?= $category->category_id ?>)"
 					   class="btn btn-primary btn-square rounded-0 center-content p-0"><?= $category->category_name ?></a>
 				</div>
 			<?php } ?>
 			<?php foreach ($items as $item) { ?>
 				<div class="col-4 col-sm-3 col-md-2 p-1">
 					<a href=#" onclick="load_item_form(<?= $item->item_id ?>)"
-					   class="btn btn-primary btn-square rounded-0 center-content p-0"><?= $item->item_name ?></a>
+					   class="btn btn-primary btn-square rounded-0 center-content p-0 btn-wrap"><?= $item->item_name ?></a>
 				</div>
 			<?php } ?>
 		</div>
