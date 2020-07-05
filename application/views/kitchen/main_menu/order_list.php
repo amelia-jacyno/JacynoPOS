679545866adceda0c492eed02334937c45678323
Adam Jacyno <jacynoadam@gmail.com>
2020-07-05 16:55:15 +0200

    ref: temp display changes for tablet and pc

--- application/views/kitchen/main_menu/order_list.php
+++ application/views/kitchen/main_menu/order_list.php
@@ -2,38 +2,38 @@
 	<div class="col-12 overflow-scroll h-100">
 		<?php
 		foreach ($order_items as $item) { ?>
-			<div class="row h-25 <?php if ($item->item_status == 'ready') echo 'bg-light-green' ?>"
+			<div class="row border h-25 <?php if ($item->item_status == 'ready') echo 'bg-light-green' ?>"
 				 id="item-row-<?= $item->order_item_id ?>">
 				<div class="col-2 p-0">
-					<img src="<?= $item->item_image ?>" alt="Image" width="100%" class="btn-square border">
+					<img src="<?= $item->item_image ?>" alt="Image" width="100%" class="">
+				</div>
+				<div class="col-2 p-0 border-right border-left">
+					<div class="row no-gutters h-50 border-bottom">
+						<div class="col-12 center-content">
+							<h1><?= $item->item_time ?></h1>
+						</div>
+					</div>
+					<div class="row no-gutters h-50">
+						<div class="col-12 center-content">
+							<h1><?= $item->order_table ?></h1>
+						</div>
+					</div>
+				</div>
+				<div class="col p-2 border-right">
+					<h1><?= $item->item_name ?></h1>
+					<h4 class="font-weight-normal"><?= $item->item_comment ?></h4>
 				</div>
 				<div class="col-2 p-0">
-					<div class="row no-gutters h-50 border">
-						<div class="col-12 center-content">
-							<b><?= $item->item_time ?></b>
-						</div>
-					</div>
-					<div class="row no-gutters h-50 border">
-						<div class="col-12 center-content">
-							<b><?= $item->order_table ?></b>
-						</div>
-					</div>
-				</div>
-				<div class="col p-0 border">
-					<b><?= $item->item_name ?></b><br>
-					<?= $item->item_comment ?>
-				</div>
-				<div class="col-2 p-0 border">
 					<?php
 					if ($item->item_status == 'ready') { ?>
 						<a id="btn-<?= $item->order_item_id ?>"
 						   onclick="item_delivered_popup(<?= $item->order_item_id ?>)"
-						   class="btn p-0 m-0 btn-success text-light w-100 center-content rounded-0 h-100">
+						   class="btn p-0 m-0 btn-success text-light w-100 center-content rounded-0">
 							<i class="fas fa-check"></i>
 						</a>
 					<?php } else { ?>
 						<a id="btn-<?= $item->order_item_id ?>" onclick="item_ready_popup(<?= $item->order_item_id ?>)"
-						   class="btn p-0 m-0 btn-success text-light w-100 center-content rounded-0 btn-square">
+						   class="btn p-0 m-0 btn-success text-light w-100 center-content rounded-0 h-100">
 							<i class="fas fa-check"></i>
 						</a>
 					<?php } ?>
