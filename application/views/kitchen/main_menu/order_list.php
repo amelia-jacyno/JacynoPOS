81127e83003b221e8dbbdc8874c2b96b5e496fc4
Adam Jacyno <jacynoadam@gmail.com>
2020-07-05 16:01:12 +0200

    fix: temp fix for iOS and old tablets for kitchen display

--- application/views/kitchen/main_menu/order_list.php
+++ application/views/kitchen/main_menu/order_list.php
@@ -2,7 +2,8 @@
 	<div class="col-12 overflow-scroll h-100">
 		<?php
 		foreach ($order_items as $item) { ?>
-			<div class="row <?php if ($item->item_status == 'ready') echo 'bg-light-green'?>" id="item-row-<?= $item->order_item_id?>">
+			<div class="row h-25 <?php if ($item->item_status == 'ready') echo 'bg-light-green' ?>"
+				 id="item-row-<?= $item->order_item_id ?>">
 				<div class="col-2 p-0">
 					<img src="<?= $item->item_image ?>" alt="Image" width="100%" class="btn-square border">
 				</div>
@@ -25,12 +26,13 @@
 				<div class="col-2 p-0 border">
 					<?php
 					if ($item->item_status == 'ready') { ?>
-						<a id="btn-<?= $item->order_item_id ?>" onclick="item_delivered_popup(<?= $item->order_item_id?>)"
-						   class="btn p-0 m-0 btn-success text-light w-100 center-content rounded-0 btn-square">
+						<a id="btn-<?= $item->order_item_id ?>"
+						   onclick="item_delivered_popup(<?= $item->order_item_id ?>)"
+						   class="btn p-0 m-0 btn-success text-light w-100 center-content rounded-0 h-100">
 							<i class="fas fa-check"></i>
 						</a>
 					<?php } else { ?>
-						<a id="btn-<?= $item->order_item_id ?>" onclick="item_ready_popup(<?= $item->order_item_id?>)"
+						<a id="btn-<?= $item->order_item_id ?>" onclick="item_ready_popup(<?= $item->order_item_id ?>)"
 						   class="btn p-0 m-0 btn-success text-light w-100 center-content rounded-0 btn-square">
 							<i class="fas fa-check"></i>
 						</a>
