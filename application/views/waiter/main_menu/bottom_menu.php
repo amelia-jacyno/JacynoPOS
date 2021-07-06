918710b0923eff062f28b484a807a3e4e0116148
jacynoadam <adam.jacyno@comote.pl>
2021-07-06 15:43:08 +0200

    hotfix: update tables

--- application/views/waiter/main_menu/bottom_menu.php
+++ application/views/waiter/main_menu/bottom_menu.php
@@ -8,12 +8,15 @@
 			<option value="3">3</option>
 			<option value="4">4</option>
 			<option value="5">5</option>
+			<option value="6">6</option>
+			<option value="10">10</option>
 			<option value="11">11</option>
 			<option value="12">12</option>
 			<option value="13">13</option>
 			<option value="14">14</option>
 			<option value="15">15</option>
-
+			<option value="16">16</option>
+			<option value="17">17</option>
 		</select>
 	</div>
 	<div class="col-6 h-100 p-0">
